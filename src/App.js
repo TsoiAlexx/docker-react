25fd7c11937065aed7e1f908b0416fb0ff8c3819
TsoiAlexx <tsoialeksei17@gmail.com>
2025-12-23 21:28:50 +0300

    action1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Sabrina!!!
+          Hello from Madina!!!
         </p>
         <a
           className="App-link"
